84df933142a39f64e657c4fe2360d1c33c5de20f
Kenna <kenna.hwa@gmail.com>
2021-10-10 22:53:19 +0900

    firebase 연결

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDFCUIJtwPMADHBM-3dBPyMOrEZ-c59ZDk",
+  authDomain: "toowitter-3a42d.firebaseapp.com",
+  projectId: "toowitter-3a42d",
+  storageBucket: "toowitter-3a42d.appspot.com",
+  messagingSenderId: "992490737891",
+  appId: "1:992490737891:web:fd9ca4765cb951510b75d6"
+};
+
+// Initialize Firebase
+export default initializeApp(firebaseConfig);
